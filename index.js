302c6a5a59c469f5c7127d93f580008ff8a6ea21
Allie LeFever <alefever39@gmail.com>
2022-05-18 09:28:52 -0600

    commit tree b6eb14c55f3544c1f1b97f82a25fcfbb1797d11c

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(array, driverName) {
+    const mathcingDrivers = array.filter(item => item.toLowerCase() === driverName.toLowerCase());
+    return mathcingDrivers;
+}
+
+// const testArray = ['Some text', 'Hello', 'Akila', 'Sensitive']
+
+
+
+function fuzzyMatch(array, driverName) {
+    const matchingDrivers = array.filter(item => (item.toLowerCase().substr(0, driverName.length)) === driverName.toLowerCase());
+    return matchingDrivers;
+    }
+
+// console.log(fuzzyMatch(testArray, 's'))
+
+
+function matchName(array, driverName) {
+    const matchingDrivers = array.filter(item => item.name === driverName)
+    return matchingDrivers;
+}
